b2aecf84aa3397b955a0f9511a5985b29ecc3a79
harry <harry@localhost.localdomain>
2019-04-16 21:19:04 +0100

    Finished grains exersise

--- erlang/grains/src/grains.erl
+++ erlang/grains/src/grains.erl
@@ -6,10 +6,10 @@
 
 square(X) -> 
     if 
-        0 >= X orelse 64 == X-> 
-            "square must be between 1 and 64";
+        X=<0 orelse X>64 -> 
+            {error, "square must be between 1 and 64"};
         true ->
-            pow(X,2)
+            trunc(pow(2,X-1))
     end.
 
-total() -> (1-pow(2,64))*-1.
+total() -> (1-trunc(pow(2,64)))*-1.  % using trunc to format the result as an integer
