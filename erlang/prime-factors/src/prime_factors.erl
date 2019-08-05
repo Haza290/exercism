7fd2e91fe309ce6465122d134291aed7163de86f
Harry <Harry.Schneider@AquilaHeywood.co.uk>
2019-08-05 17:11:27 +0100

    Fixed next_prime but needs refactoring

--- erlang/prime-factors/src/prime_factors.erl
+++ erlang/prime-factors/src/prime_factors.erl
@@ -3,7 +3,7 @@
 -export([factors/1,next_prime/1]).
 
 
-factors(X) when X <= 1-> undefined;
+factors(X) when X =< 1 -> undefined;
 factors(X) ->
     factors(X, 2, []).
 factors(X, X, PrimeFactors) ->
@@ -14,11 +14,11 @@ factors(X, PrimeNumber, PrimeFactors) ->
     factors(X, next_prime(PrimeNumber), PrimeFactors).
 
 next_prime(X) ->
-    next_prime(X, 2).
+    next_prime(X + 1, 2).
 next_prime(X, Y) when X / 2 < Y ->
     X;
 next_prime(X, Y) when X rem Y == 0 ->
-    next_prime(X+1);
+    next_prime(X);
 next_prime(X, Y) ->
     next_prime(X, Y+1).
 
